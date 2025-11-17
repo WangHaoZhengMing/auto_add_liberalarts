6700b41aa5d041614eca81bbecea0b19a94dfe08
Your Name <you@example.com>
2025-11-17 15:36:39 +0800

    1

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ async def main() -> None:
         ports=[2001, 2002,2003,2004,2005,2006,2007,2008,2009],
         zujvanwang_catalogue_url="https://zujuan.xkw.com/czyy/shijuan/bk/a310000")
 
-
-    # 初始页面，可以是一个空白页或登录后的页面
-    # initial_url = "https://tps-tiku.staff.xdf.cn/tasks/questionSetCreate"
-    # 启动所有浏览器实例
-    # open_muti_browsers(ports, initial_url)
     
     # 为每个端口和URL创建一个并发任务
     tasks = []
     # Filter out invalid URLs
     print("找到的题目页面URL列表:{config.zujvanwang_questions_urls}")
+
+
+# https://tps-tiku-api.staff.xdf.cn/paper/check/paperName?paperName=%E8%BF%99%E9%87%8C%E6%94%BE%E8%AF%95%E5%8D%B7%E5%90%8D&operationType=1&paperId=
+# i want you to test if the testpaper is exist in our bank. post a request with the paper name to the above url. if the response show that the paper is exist, then we can skip this paper.and print the paper name to ./重复.txt
+
+
+
     valid_urls = [url for url in config.zujvanwang_questions_urls if url.startswith("http")]
     
     # Use the minimum of the number of ports and the number of valid URLs found
